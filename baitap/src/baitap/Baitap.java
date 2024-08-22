039141ec279f1819704b666392fcd105294f6b8c
JohnnNguyen <nguyenquangvu0207@gmail.com>
2024-08-22 11:15:45 +0700

    giới thiệu

---
+++ baitap/src/baitap/Baitap.java
@@ -0,0 +1,30 @@
+package baitap;
+import java.util.Scanner;
+public class Baitap {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your name: ");
+        String name = scanner.nextLine();   
+        System.out.println("enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("enter your sex: ");
+        String sex = scanner.next();
+        scanner.nextLine();
+        System.out.println("enter your major: ");
+        String major = scanner.nextLine();
+        System.out.println("enter your GPA: ");
+        float GPA = scanner.nextFloat();
+        scanner.nextLine();
+        System.out.println("enter your country: ");
+        String country = scanner.nextLine();
+        System.out.println("\n--- User Information ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Sex: " + sex);
+        System.out.println("Major: " + major);
+        System.out.println("GPA: " + GPA);
+        System.out.println("Country: " + country);
+    }
+    
+}
+    
